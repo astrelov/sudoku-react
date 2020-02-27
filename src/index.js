4d1ef34b40ca424d6a9e5332389098fb5759ad7e
astrelov <krendelllllll@gmail.com>
2020-02-27 09:46:13 +0200

    initial

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Image } from './js/components/Image'
+
+ReactDOM.render(<Image src="./src/resources/img/controls/easy_c8c8c8.png" className="no-events"/>, document.querySelector('#container'))
